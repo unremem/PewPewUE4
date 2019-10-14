f96f704ac4b8cef7a966dbbf7d8082a9623fe547
Unremem <unremem@gmail.com>
2019-10-13 23:48:16 -0700

    Missed the source files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# PewPew
+
+Developed with Unreal Engine 4
---
+++ Source/PewPew/PewPewGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PewPewGameMode.generated.h"
+
+UCLASS(MinimalAPI)
+class APewPewGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APewPewGameMode();
+};
+
+
+
